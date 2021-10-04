7f4f82266efe25eef88a4f355f661f69ef49849e
BoneGames <benjaminjgbarnes@gmail.com>
2021-10-04 20:00:13 +1100

    routing to info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import NewsTicker from './components/ticker';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-     <NewsTicker />
+    <Router>
+      <div>
+     <Switch>
+     <Route exact path="/">
+       <NewsTicker />
+     </Route>
+     <Route path="/about">
+       <About />
+     </Route>
+   </Switch>
+ </div>
+</Router>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About MoB</h2>
+    </div>
   );
 }
 
--- src/components/ticker.js
+++ src/components/ticker.js
@@ -4,12 +4,15 @@ import './ticker.css';
 import './button.css';
 // import { useTextWidth } from '@imagemarker/use-text-width';
 import headlinesText from './headlines';
+import {
+    Link
+  } from "react-router-dom";
 
 
 
 function NewsTicker() {
     // const fullTextWidth = useTextWidth({text: headlinesText, font: '3em monospace'});
-    const fullTextWidth = 200000;
+    const fullTextWidth = 9845793475;
     // this is async it seems - will be easiest to calculate value of this once when final text file is ready
     // then include as hardcoded var as shown aove
 
@@ -21,16 +24,17 @@ function NewsTicker() {
 
     // will need to set this as literal time when ticker goes live
     // currently is set to have started 30 seconds ago
-    const startTime = Date.now() - 30;
+    const startTime = Date.now();
 
     const timeStamp = Date.now();
     const _offset = _speed * (timeStamp - startTime) * charWidth * -1;
 
-    // console.log("charCount: " + charCount);
-    // console.log("FullTextWidth: " + fullTextWidth);
-    // console.log("charWidth: " + charWidth);
-    // console.log("offset: " + _offset);
-
+    console.log("charCount: " + charCount);
+    console.log("FullTextWidth: " + fullTextWidth);
+    console.log("charWidth: " + charWidth);
+    console.log("offset: " + _offset);
+    console.log("NOW TIME: " + timeStamp);
+    
     console.log(_offset);
     return (
         <div>
@@ -44,7 +48,7 @@ function NewsTicker() {
             )}
         </Ticker>
         {/* INSERT MROUTING PATH FOR LINK TO INFO BELOW - href */}
-        <div className="linkButton"><a href="">info</a></div>
+        <div className="linkButton"><Link to="/about">About</Link></div>
         </div>
     );
 }
