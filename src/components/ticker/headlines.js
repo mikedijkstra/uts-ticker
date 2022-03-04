47f53e333c8d669d4ca5c26c9db55fdbf918644a
makeorbreakart <makeorbreakart@gmail.com>
2022-03-04 11:23:08 +1100

    Testing headline update

--- src/components/ticker/headlines.js
+++ src/components/ticker/headlines.js
@@ -1,155 +1,89 @@
 // Set to change symbol that intervenes between headlines
 const delimiter = "  //  ";
 
-const headlinesText = `‘Compassion’ behind hospital visit rule overhaul
-‘Hit job’: Rogan’s claim over Spotify drama
-‘I quit my job and turned $70 into millions’
-‘Players love him’: Big rally for Langer’s replacement
-‘Shock’ birth side-effect no-one talks about
-‘Slob’: Groom’s wedding outfit torn apart
-$60m jackpot up for grabs
-A-League bracing itself for new normal
-Adele was the only one surprised when she won big at the Brit Awards
-After the world's most awkward photo-op, Morrison is being haunted by a string of other unfortunate images
-AGL plan to stay in coal until 2040s angers environmental groups
-All the coronavirus news you need in the one place
-American's "British tea"; method sparks outrage
-An Australian is reportedly on the move ahead of the NBA trade deadline, but it's not Ben Simmons
-Anne Neill was an inconspicuous 1950s housewife. She was also an undercover spy
-As WA's new school rules spark confusion, you get the feeling it's going to be a long first term for everyone
-Assange supporters spend $72 million on digital art to help fund his legal defence
-ASX CEO set to resign
-ASX gains, AMP reports loss, NAB margins down, AGL to close power stations early
-ASX lifts as travel stocks keep flying
-Aussie Belle Brockhoff fourth in women's snowboard cross final
-Aussie cruise ban to be reconsidered by expert body
-Australia Day Council not behind 'threatening' phone call to Grace Tame, says Liberal senator
-Australia is reopening to tourists. Here's who can travel, and when
-Baldwin breaks down: ‘I’m very sad’
-Balloons kill wildlife and end up as litter', says environmentalist as she urges NSW to ban their release
-Banks weigh on ASX 200 as ANZ outlook disappoints, travel stocks jump on border reopening news
-Big bank profit pushes ASX firmly higher
-Big names cut from Sydney’s power list
-Burnt body found at Gold Coast as police quiz second man with burns
-Caitlyn Jenner gushes over Kylie’s new son
-Cambodian prime minister admits 'mistake' and says Australian economist still detained in Myanmar
-Canadian-backed beef producer rounds up 1.1m hectares in NT
-Changing regional media: Is it the death knell for print or an opportunity?
-Chaotic sights leave 'crab cam' viewers pinching themselves
-Clashes as New Zealand police clear Covid protest
-Collective shiver' among WA aged care workers as union issues warning after first COVID cases
-Colleen trekked Victoria's coastline to pick up rubbish, what she found was 'horrendous'
-COVID would be 'unmanageable' in WA if border opening delayed beyond May, new modelling shows
-COVID's spread in WA is threatening Australia's $155 billion export. Here's what the miners are doing to try and keep running
-Destroyed': Afghanistan's health system crumbles as COVID-19 wave surges
-Diesel price premium pinches mango margins
-Djokovic included in Indian Wells start list despite 'proof of full vaccination required for entry'
-Dom hedges on tourist booster jab decision
-Donald Trump tells Joe Rogan to 'stop apologising' to the mob
-Earth's core is neither solid iron, nor liquid. It's a whole lot weirder
-Eight years after a hail storm, Kendra is living in an 'uninhabitable' home feeling abandoned by insurer
-Eight years and two trials later, Victorian mechanical company fined $210k after triple fatal
-England looks to fast-track plans to scrap self-isolation after positive COVID-19 test
-Everything you need to know about the 2022 Super Bowl halftime show
-Fact check: Did Richard Colbeck attend the Senate's COVID-19 hearings 'on countless occasions'?
-Fierce, formidable, fearless: Tame and Higgins stormed the Press Club with a message for Scott Morrison and the nation's leaders
-First the birds, now frogs show Macquarie Marshes drought recovery in full swing
-Flu 'worse than COVID' in children, Australians 'susceptible' and urged to get another jab
-For Fiona and her family, COVID lockdowns are here to stay
-Former pope Benedict XVI seeks forgiveness for his handling of sexual abuse allegations in German Catholic Church
-Friends star out with rarely-seen boyfriend
-From 3,000 per cent growth to 'worse than lockdowns' — the winners and losers of the Omicron outbreak
-Getting inside Putin's head 'an impossibility';
-Global COVID cases surpass 400 million
-Google Maps to fix directions after years of trapped wet-season travellers
-Government consulting religious groups on next steps after defeat on discrimination bill
-Grace Tame's abuser was convicted of this offence. Now the ACT government is seeking to change it
-Horror as mum found chained in China
-House prices and rents could be impacted by international travel, but in different ways
-How to live stream Australian water polo league, tournament guide
-How to watch latest Australian Water Polo League matches
-I didn't fully understand': Paul invested thousands in crypto through an app — days later it was gone
-I get goosebumps': World-first wallaby relocation project bounds along
-I thought it was a pimple': Why Uncle Fred is urging people to have check-ups
-In-form Demon back where he belongs
-It feels like flying': Valentino Guseli set to soar after reaching Olympic snowboard halfpipe final
-It looked like blood but it was actually honey': Perth couple confront surprise substance oozing from wall
-Jimmy bought a house on Sydney's outskirts — he has a warning for others about to move there
-Kanye pleads with Kim in desperate post
-Kanye West is in an ‘open’ relationship
-Kelpies shine in ABC TV's Muster Dogs but if not stimulated 'they'll work ants', says dog trainer
-Kidman Leads Aussie Oscar Nominations 10/2/22
-Labor MP set to quit over corruption probe
-Labor reveals next move on controversial bill
-Let it rip': Have COVID-19 policies impacted your work or business? We'd like to hear from you
-Live: All the coronavirus news you need in the one place
-London to Nashville: Slash says rock’s rolling on
-Long-delayed Poirot sequel is thrown overboard by presence of disgraced star Armie Hammer
-Man dies in ‘suspicious’ house fire
-Merc driver’s alleged high range drunken pursuit
-Model: ‘Why I was right to remove breast implants’
-Morrison flying like a downhill skier to the finish line — but he's juggling a chainsaw on one ski
-Mother of eight found chained in shed
-National Cabinet to meet this afternoon
-Neighbours star calls on PM to save iconic soap
-New money theme parks an adrenalin ride for investors
-New York latest to roll back mask mandates as US eyes normalization
-Nic’s Oscars nod: ‘I literally cried’
-NSW and Vic coal fired power stations to close early
-NSW, Vic record 40 Covid deaths, 20K new cases
-NSW’s deadliest beach drowning hot spots revealed
-Online retailer to expand already huge range
-Playful Wombat Zooms Around Carer&#8217;s Canberra Yard
-Preserving military history: Honouring the legacy of Torres Strait's Horn Island servicemen of WWII
-Rarer than pandas': New tree species discovered in Gold Coast backyard
-Red Bull unveils new car Verstappen will drive to defend F1 world championship
-Regional aged care home denies 'chemically restraining' dying great-grandmother
-Russian media reports teen figure skater tested positive for banned drug
-Samsung’s new Galaxy S22 to rival Apple: What Aussies will get
-Sarah lives with disability and the trauma of abuse. Her experience is alarmingly common
-Secret haunts of Sydney celebs revealed
-Senator slams ‘selfish’ bill ahead of upper house debate
-Shocking graph exposes cafe crisis
-Some like it hot: Sydney’s sexiest restaurants
-Star ‘fell in love’ with mystery Aussie
-Star couple’s bizarre parenting story
-Star posts shocking sex act online
-Surprising items Aussies are buying
-Tahlia has been homeless most of her life. A derelict caravan park may change that
-Tai Tuivasa's clash with Derrick Lewis at UFC 271 will be fighting at its purest
-Taken for granted': Pharmacists pull out of vaccinating young children and free RAT programs
-Teacher’s racy outfit on night of student sex
-The government lost a dramatic showdown on religious discrimination laws overnight. What happens next?
-The impact of Australia's closed borders: These four graphs tell the story
-The world will be able to live with COVID-19', but experts warn life might still look very different for years
-There's one thing the bank can't ask you when applying for a loan
-They don't even fit the kids': Back-to-school packs ridiculed
-This year's Oscar nominations have been released — here's who's in and who got snubbed
-To watch Johnny Raper play rugby league was to know you were watching one of the greats
-Travel stocks rocket on new border rules
-Trolls slander grandparents of dead baby
-Trudeau slams Canada trucker protests, police threaten arrests
-Truth about stars’ 35-year age gap marriage
-UK Military Shipments Arrive in Ukraine as British Foreign Secretary Travels to Moscow
-UK police review Downing Street event
-US Secretary of State Antony Blinken touches down in Melbourne
-Vegemite’s new flavour and look
-Victoria records 9391 cases, 16 Covid deaths
-Victory or defeat in snowboard cross can hinge on any one of a multitude of insane moments
-We fact checked Anne Ruston on the government's federal integrity commission. Here's what we found
-We need a new style': Cummins breaks his silence on Langer's resignation
-We've got a Quad, what about a quintet? The powerful country noticeably missing from crucial talks
-Wedding wager on Birdsville Cup win may get Brophy to the altar, and the odds are narrowing
-What has kept MPs up all night
-What it's like being a woman that works as a bricklayer
-What you need to know before buying Crypto
-When will children under five be eligible for a COVID jab?
-Who are Sydney’s most powerful celebs?
-Who didn’t make Sydney’s power list in 2022?
-Why it’s harder to find a bank branch
-Why this Indigenous All Star lived in a pub 1400km from home to chase her rugby league dreams
-Will I need a fourth dose of a COVID-19 vaccine? Experts weigh in on who will need a further booster shot
-Woman falls from balcony at aged care home at the centre of Queensland's deadliest COVID outbreak`;
+const headlinesText = `Cynical emojis give too many *clapping emoji* *clapping emoji* *clapping emoji* to banks
+Which do we *heart emoji* more? Movies or TV?
+COVID isolation sees increase in empathy
+COVID home deliveries of food sees people learn whose Country they are living on
+First Nations storyteller acknowledged as best in the country
+Rockdale reviewed: pass the parsley
+Australian tenants ranked by postcode
+"What a force she was": tributes roll in for Wes Anderson.
+We love you Australia!
+The Big If: Bank Australia goes fee-free
+Azuba Japanese 🏃🏽‍♀️ from Bondi Junction as lovely Serena becomes new owner.
+YSL rises in popularity as Em coins it as "hot stuff"
+Pretty girl Damian steps into Partyland Bondi Junction in a bid to *running emoji* from Netflix producers.
+Gorgeous Milly collaborates witb Urban Decay as the brand expands to Peru
+Hot Stuff Will Gerstl supports Citizen J workshop marking the commencement of his UTS attendance
+*sparkle heart emoji*  is in the air as Isla and Serena are spotted together at Azuba Japanese
+For Serena and her stunning family, Netflix deals loom right around the corner.
+Pretty Peruvian citizen Damian shares his *arrow heart emoji* for his country in this tell all interview.
+Westfield bondi junctions stuns yet again with introduction of new YSL store
+Citizen J deemed "Hot Stuff" by recent participant Damian Norris
+Big names cut from Bondi Icebergs' artist list
+First the *fire emoji*, now sauna show github drought recovery in full swing
+Shocking apple exposes publication crisis
+Why it’s harder to find a dance clan
+We access a new art': Jess breaks his silence on *jazz hands emoji* resignation
+Trolls slander sis of beautiful *heart emoji*
+UTS lives with dropbox and the trauma of teams. Their experience is alarmingly common
+Personal icebergs 'worse than COVID' in Sydney, Australians 'personal' and urged to get another *bicep emoji*
+Chaotic content leaves '*kiss emoji* cam' viewers excusing themselves
+What artists need to know before dancing beautiful tech support
+Vale Neil Balnaves whose contributions to philanthropy were essential
+Ranked: the best backpacks for cats
+Moon phases illuminate new baby fortunes
+Who are Sydney’s most powerful new babies?
+Will I need a fourth dose? Experts weigh in on who will need a further flopping fish cat toy
+Global Guillain Barre syndrome cases surpass 400 million
+Diesel price premium pinches moon margins
+For Fiona and her family, human anatomy is here to stay
+Why Australians can't get enough birthday wishes *heart emoji* *heart emoji* *heart emoji*  *shooting star emoji* *sunflower emoji*
+Caitlyn Jenner gushes over Kylie’s new backpack
+Sick of takeaway? Try this amazing Bao Buns recipe
+Why people are not happy with wordle recently
+‘This is so cute,’ said no one after tuning into witnesses accounts at the Ben Roberts-Smith defamation case
+How the university enrolment process is harder than the actual course, an opinion piece
+Kim Kardashian pleads ‘this is not funny’ with Kanye to leave family alone on social media 
+The truth behind these famous photos may amaze you
+An updated list of movies to make sure you never have the awkward “I’ve never seen it” talk again
+Everything you need to know about the COVID-19 vaccination booster
+Candid is the new cute according to tik tok
+Kanye West drama continues, will Kim become a Swift?
+Getting inside Lin-Manuel Miranda's head is an impossibility
+Vote on questionable Kanye tweets
+19 most toxic men of 2022
+Anna Delvey- Capitalism and Ego Formation
+I tried Pinterest and my skin has never been more thankful
+Labour MP set to quit over leaked buzzfeed quiz results
+Horror as mum watches euphoria for the first time
+Shocking research- earth's core is made of nandos
+14 times Drag Queens crocheted the New Zealand flag.
+Emmett's bag of eggs attends Mardi Gras, parodying Robert Pattinson.
+UTS requirements in 2022 calling for 3 minimum types of internet.
+Centrelink calls Jimbo the Drag Clown a 'butch stockinette stitch'. Blu Hydrangea responds.
+Cow in balaclava illegally streams Heathers movie.
+Trans 'einstein' makes other MacOS Sims 3 VPN.
+Heathers shapes Mardi Gras, GQ reports.
+Autumn stream reaches minimum requirements for Drag Race session.
+3 Stockinette Stitches You Can Use For Parodying English Queen.
+This Sunday UTS will make history parodying their timetable, comparing Student Centre phone lines to Centrelink call centre.
+Bob Carr wants to turn all the golf courts in Sydney into wilding parks
+Sydney Opera House is not the only architectural symbol of Sydney
+Many UTS students think Faichun is beautiful and easy to write
+Congratulation on the successful event of CitizenJ Workshop
+UTS students are rich!
+Taiwanese politicians think Australian government attracts too many domestic students to the land to study
+Fencing equipments will be offered to UTS students for free
+The mentoring program getting many supports from UTS and its alumni
+NSW government intends to use more advanced technology to control the unpredictable weather in the city
+"God is NOT in the details" someone argues against Mies
+"whats the law got to do with me?" and more questions riddling Australian citizens
+PM's New album described as "good" by supportive media
+What does 'punitive' mean? Confused nation forgets.
+Are pets real? UTS research suggests otherwise...
+PM's bad hair day: "You have got to stop commenting on my appearence"`;
 
 // The second 2 replace all's get some wierd utf-8 misnomers that fuck up how single quotes are displayed
 export default headlinesText.replaceAll("\n", delimiter).replaceAll("&#8216;", "'").replaceAll("&#8217;", "'");
