1273c0cbd0f754f97cfce4b85e433b813841ab41
makeorbreakart <makeorbreakart@gmail.com>
2022-03-18 13:51:37 +1100

    Testing headline update

--- src/components/ticker/headlines.js
+++ src/components/ticker/headlines.js
@@ -1,199 +1,23 @@
 // Set to change symbol that intervenes between headlines
 const delimiter = "  //  ";
 
-const headlinesText = `There is less space for people to live
-News services use your data as currency in 2062
-When we are facing urgent issues locally, our brains can't think globally.
-The things that are bad in 2062 will be different to the things that are bad in 2022
-Hybrids and generateds are subject to failures and blowouts that can take weeks or even months to repair depending on the state of global supply chains.
-2062 predictions for wildfires (31% risk), storms and flooding (47% risk), and cyclone activity (5% risk) in your local area.
-We have to respect it – there is no alternative.
-Let’s argue: practical skills more important than theory
-Two upright rectangular boxes with a screen and three round dials: how AI 'sees' your images in 2062.
-An otherwise pleasant March 2062 Monday morning in the Central Business District, sweaty traders in suits they probably slept in piled out of their classic roadsters.
-Media bias is more prominent in 2062 due to automation
-By 2062, former Australia has separated into smaller countries again
-"I am thankful to the Earth that she was kind and left us untouched after we fled in 2044." 
-2062 years of living stashed away in the corner. 
-Pay no attention to the outdoors
-In this growing place I can feel the heat of the arid zone. 
-In a media presentation on 9 March 2062, the head of the Union for Human, Hybrid and Generated Workers (UHHGW), Ostrid Seven, put what some say is the nail in the coffin for human-to-human service industries.
-By 2062, plastic and clothes production have been uprooted at a rate that they cannot be reinstated fast enough to keep up with global demand
-Old political issues and fears continue to cycle through
-"We’ve carved our place here. We're settled."
-Flesh-mob leader Hurlstone Hicks said “we will continue to stand on our own two feet, bones and tendons, muscles and flesh, and refuse to be replaced by mutants and robots.”
-In the 2050s and 2060s, celebrity is path to class mobility and economic success
-CRYPTOCURRENCY CRASHES, BRINGS AUSTRALIA TO ITS KNEES, FORCES LIBERAL PRIME MINISTER TO SELL HAWAIIAN MANSION
-He checked the Sydney Midmorning Prophet.
-How to tell if local news is correct
-The move to shut down human-to-human services is strongly opposed by traditionalists.
-With mass migration, new dialects develop
-Screams of “WHAT COIN?” erupted as traders scrambled to fill their now barren digital wallets with some scrap of value.
-At the end of the day, Euro-Am always has the rich end of town to sell to.
-Key technologies fail in natural disasters
-"Breathing is hard, but at night I grow." 
-Diversifying local gene pools
-When there is a different cultural makeup, the government changes to reflect this
-The weather is getting more extreme and unpredictable
-Flesh-mob protests have become more frequent and vociferous as the UHHGW moves to clamp down on businesses that refuse to include hybrid or generated workers into their workforce.
-Will it ever be safe? 
-Of its 40 million inhabitants, over half say that their number one concern is boredom, and that the best solution is moving away as soon as possible.
-Eco-fascism means that people try to dictate who can have children in 2062
-I am a grain plant who who has never felt the wind
-It’s unclear whether we’ll be able to go back to 2062. 
-Inhabitants primarily travel via hydrocars and the most common job is machine performance monitoring.
-Things are far better than they were before. 
-By 2062, a new hybrid religion has arisen that takes parts from all the former major religions
-There is innovation in deep water living systems
-Hicks claims that the slow decay of the human body is preferable to the long or endless lifespans of hybrids and generateds.
-The crowd was simply standing, staring, as each coin’s value, beside its name, carried the one symbol that they dared not speak of: the minus.
-Imported genetic material becomes increasingly common in 2062
-In America in 2062, news sources are less important
-We are developing the technology for space travel and living elsewhere
-Clutching an electronic device: generations on from the smartphones of 40 years ago.
-By 2062, word of mouth has become the only reliable local news source in communities
-Will we remain human in 2062?
-A gender non-specific yellow
-How to reduce, reuse, recycle nuclear weapons in 2062
-He was selling his Hawaii mansion, split the whole thing as a limited coin and whacked it in the blockchain.
-Catastrophic weather events have become more frequent and created a climate catastrophe around the equator
-Although there are few industries that still run solely human-to-human, they take up disproportionate communal resources for slower task processing and human error.
-We have to reuse, mend, repair and wear old clothes in 2062
-How storytellers became new sources.
-As the weather became more extreme, populations moved away from the equator by 2062
-Religion is about bringing people together in person
-People have moved to sustainable farming out of necessity
-First Nations communities help us adapt to the land and ensure that we knew how to respect it. 
-Automation is a response to the unreliability of a 2062 human workforce
-People are linked together in a circle. Some cluster together closer than others, and there is an opening at one side, suggesting that others may join this group.
-Are your eyes more important than fact checking?
-How 'independent' is independent media in 2062?
-Current economic patterns and the future.
-People want unique, one-of-a-kind things that can’t be mass produced
-Political commentators have been quick to point out that Euro-Am is a major donor to several political parties across the globe including Australia’s Neo-Liberal Party.
-Cohesive communities through culture sharing
-Most of the housing is built on stilts and family units are now typically made up of found members.
-Every coin, every single coin was worth negative of itself. An impossibility.
-In 2062, children are increasingly regarded as community raised
-There is an urgent need for sustainable transformation
-Corporations are our content moderators
-The streets of several major capital cities across the globe have erupted over the Euro-Am conglomerate’s recent decision to cut 90% of its human workforce in favor of automated technology.
-Crypto trading could be done totally and utterly from the comfort of your own table or sunbed.
-The top news story this week is the release of the Apple Home 2 and citizens access the news through OakTree.
-Looking into a VR headset we see cogs turning in the corner of a room.
-"I feel nostalgia for 2020." 
-Class divide continues to grow
-Political issues of 2022 are emphasised in 2062
-Fast fashion and throwaway products are in short supply, so we have to work with existing stocks
-People are questioning their place in the world
-What happens now we've lost our our jobs
-At least their superfunds were secure, right?
-Data is currency.
-Knowledge is shared through online sources rather than humans
-Replaced again by metal and concrete.
-Fossil fuels are done
-Severe weather conditions destroy all
-How the news differs by Country and why.
-The silence was broken when another trader called out something about the Prime Minister.
-They enter the warehouse affectionately named the piggy bank, named for the metaphorical coins discussed within and the likeness most traders resembled to the animal.
-There is less heteronormativity
-In 2062, a new kind of family unit is emerging as population continues its decline.
-More urgency on changing the climate for the better
-It was a known fact he’d been in and out of the Piggy Bank a couple of times.
-The multi-national company, which owns several major businesses such as GoFoodz, Exero-tech and Trans-Global Air, employs approximately 1 million people across the globe.
-The huge screen, mainly reminiscent of Wall Street’s classic, would typically have traders clamoring underneath, chatting, laughing and sneaking gasping gulps from dented flasks from their coats.
-There is an unemployment crisis due to automation
-The news of 2062 is driven by the desire for reader loyalty
-Metal beeps replace the sounds of water against leaves. 
-Corporations have become larger and have greater influence
-Cassius Giax, host of the “The Watch” news show on YouTube, averaging approximately 100 million viewers per episode, discussed in depth that despite the controversial move made by Euro-Am.
-When you move somewhere, you have to adapt
-Arresting the decline
-"We need to evacuate earth as soon as possible." 
-End of the human workforce force? - Euro-Am unemployment crisis comes to a head
-It was no longer habitable there and that they had no claim or stake to that land. 
-Political biases are more obvious by region or by area
-In 2062, envrionmental pressures affect how much news we read.
-Your part-time uni job in 2062 is monitoring the machines that are serving customers
-In 2062, Sydney still infamous for its creative technology. 
-Coming in every morning to chat, avoid eye contact and start drinking at 11:30 was the only reason David had to talk to his fellow Coinholers.
-Authentic relationships valued most in 2062 global survey.
-There is a cult of technology
-In 2062, publishers prioritise data acquisition over profit
-The sector has been in serious decline over the last decade.
-Through the 2040s and 50s, our relationships have been tested and come out much stronger
-Riots in the streets, and not the cool-postmodern-activist kind.
-Very little change is likely to come from protest movements due to the large class divides currently experienced across the world.
-Advances in VR education provide an immersive, individual approach to education which provides students of all ages the specific training they need to contribute to their societies.
-The world is getting much more connected
-Any expectation of an industry that is run entirely by humans for humans is unaffordable, inefficient and discriminatory.
-The air consumes me as friends start to vanish.
-There has been a shift to more authentic relationships
-Genetic studies reveal natures impact on skill adaptivity.
-"We will not have it taken from us again."
-Faith is more malleable
-Waiting for the weekly rations of me. 
-Since the automation boom of the mid-2040s, the job security of many of these employees have hung precariously in the balance.
-Their hunger is different from our hunger. It is never sated.
-Standardisation of emergency and trauma responses has killed social work and crisis outreach.
-Want to have a career? Here’s how.
-Are you still a human?
-There is a shift towards staffless operation
-Pursuing passions and side projects is valued in 2062
-In 2062, we have plans to leave Earth because things are so bad
-They emphasised the discriminatory exclusion of more than half the world’s population that these workplace pracatices represent.
-Waiting to witness the fate of the human workforce.
-A drone flies over a landscape with just a single cloud in the sky.
-It seems that the workers' security movements, that rapidly accelerated just over a decade ago, have proven futile in the long-term.
-Coming in every Monday, everyone knew, was simple pleasantries.
-​​"If you want to work with people, you have to get used to the idea that you might not get paid for it"
-What is human anymore in 2062?
-Increasing productivity and creating a more efficient society.
-David walked through the sliding doors, slightly cringing at the piercing squeal of the plastic door pulling itself open.
-New quirky behaviours emerging.
-Some people still refuse to accept other cultures and races, and this is why they're wrong
-How your local truth verification system works.
-The official occupation title was originally Coinholder but a typo at the ATO set it on a different course.
-The people in charge around the really bad point in the 2040s think about sustainability and mitigating damage towards 2062
-Humans have become obsolete
-The time has come for me to part with the land and soil which sustained me. 
-People want to terraform the environment back to its former glory
-Very few comments have been made from world leaders.
-Celebrity culture = leadership
-We see the rise of 1 person media empires in the 2060s.
-If you want to work with humans, you have to get used to the idea that you might not get paid for it. 
-Protesters have taken to the streets in a desperate bid to lobby governments across the world to implement human employment quotas.
-My genetics can remember the year 1945
-Image of four screens floating above a control panel of round and square buttons. Each screen shows a different flight or movement path.
-In the year 2062, Sydney (now known as Sydnex) is (in)famous for its permanently flooded city.
-They slept in classic 2001 Toyota Camrys or the consistent crowd-pleaser, the 2004 Mitsubishi Mirage.
-The wealthy are able to define their family units
-There is more time for hobbies
-It’s pretty inconsequential what the impoverished masses want - that’s not who they’re selling to anyway.
-The highest paying job of 2062: machine monitoring
-Half of Sydney say that their number one concern for 2062 is the environment. 
-It will have to get a lot worse before it gets better
-Political divide continued to grow
-The focus is on survival
-Perhaps that the Australian government had no choice but to take us? 
-The climate crisis of 2062
-Truth depends on your location
-Sudden mass migration led to changes in cultural makeup
-Racists are getting bashed.
-People from the same culture tend to stick together
-Machine faults rise with bad weather
-There has been a change of power from a white majority to a more diverse government
-Technology has more influence on young people than parents or family
-Language groups cross-pollinate
-The world continues to watch, with the eyes of millions turned to the doorsteps of countless parliament buildings across the globe, waiting for the leaders of the world’s next move.
-They were usually placated by the array of various coin names on the main screen inside - Doge, Safemoon, Ethereum and the like - and the numbers that regular, non-traders simply couldn’t understand.
-Our identity is based on technology
-Verification services overtake news content production in 2062.
-How to tell if you’re still a human
-Social authenticity is harder to create, but so much more valuable in 2062
-Large scale production isn’t possible anymore in 2062
-Self-serve cancelling bots proliferate
-Ideology in 2062 is more important than profit`;
+const headlinesText = `Asteroid mining company denies it is running sweatshop on the moon
+Scientists discover new way to make a black hole: just don't pay your rent for 10 years
+Luxury employment vs permanent retirement
+Gorilla sighted after 27 years, now living in SimCity
+Slam poetry goes AI: first AIs to compete in tournament
+Study finds that over half of people who say "I can't believe this is real life" are just pretending to be surprised
+AIs don’t have gender, or do they?
+Studies now revealing the decaying neural system and increasingly corrupt consciousness
+Copyrights dissolved? New controversial laws repealed
+Synthetic water replacement actually found to be sentient?
+New Hypnoreal Bodysuits set to save thousands of on-site militia
+Dolphins all dead: Marine biologist fails to save species from extinction
+How to be an intergalactic diplomat
+Neuroscientists believe that the ability to predict the future is not an innate skill but can be learned
+First Gen Inc. launches their new, innovative, cyber-organic modification for optimistic Sci-heads!
+The Microchip Spikers: how to protect your identity from thieves
+Space Capitalism sends stock market into volatile freefall`;
 
 // The second 2 replace all's get some wierd utf-8 misnomers that fuck up how single quotes are displayed
 export default headlinesText.replaceAll("\n", delimiter).replaceAll("&#8216;", "'").replaceAll("&#8217;", "'");
